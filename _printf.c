29573023ddb98597eddce55a1d96ac43b95ec4a0
Alejandra Hincapie <alejandra612@hotmail.com>
2021-07-28 16:36:02 -0500

    correccion de if

--- _printf.c
+++ _printf.c
@@ -15,8 +15,17 @@ int _printf(const char *format, ...)
 
 	va_start(list_argument, format);
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	if (!format)
 		return (-1);
+	else
+	{
+		if(format[0] == '%')
+		{
+			if (format[1] == '\0')
+				return (-1);
+		}
+	}
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
