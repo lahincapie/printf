e02d2863585191aa0656d5ca2200767cba5c7181
Alejandra Hincapie <alejandra612@hotmail.com>
2021-07-28 10:23:25 -0500

    correccion

--- _functions.c
+++ _functions.c
@@ -34,11 +34,7 @@ int print_string(va_list list_argument)
 	/* si s es igual a NULL */
 	if (str == NULL)
 	{
-<<<<<<< HEAD
 		str = ("null");
-=======
-		str = "(null)";
->>>>>>> 49d98015f169492d6e3d4ddf3f744e6d09d96130
 	}
 	/**
 	 * para i igual a 0; mientras que s en la posicion i sea
